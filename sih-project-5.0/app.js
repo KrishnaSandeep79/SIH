14e99cbb862746715ee31944526d7c68739a8ab0
Krishna Sandeep <chittepusandeep1@gmail.com>
2020-05-08 10:12:54 +0530

    Final Commit

--- sih-project-5.0/app.js
+++ sih-project-5.0/app.js
@@ -1,15 +1,41 @@
-var express = require('express');
-var signup = require('./controllers/signupcontroller');
-var login = require('./controllers/logincontroller');
+// import express JS module into app 
+// and creates its variable. 
+var express = require('express'); 
+var app = express(); 
 
-var app = express();
+// Creates a server which runs on port 3000 and 
+// can be accessed through localhost:3000 
+app.listen(3000, function() { 
+	console.log('server running on port 3000'); 
+} ) 
 
-app.use('/assets',express.static('./public/assets'));
-app.set('view engine','ejs');
+// Function callName() is executed whenever 
+// url is of the form localhost:3000/name 
+app.get('/', callName); 
 
-signup(app);
-login(app);
+function callName(req, res) { 
+	
+	// Use child_process.spawn method from 
+	// child_process module and assign it 
+	// to variable spawn 
+	var spawn = require("child_process").spawn; 
+	
+	// Parameters passed in spawn - 
+	// 1. type_of_script 
+	// 2. list containing Path of the script 
+	// and arguments for the script 
+	
+	// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
+	// so, first name = Mike and last name = Will 
+	var process = spawn('python',["./vce_login.py", 
+							"1602-17-733-079", 
+							"skiing"] ); 
 
+	// Takes stdout data from script which executed 
+	// with arguments and send this data to res object 
+	process.stdout.on('data', function(data) { 
+		res.send(data.toString()); 
+	} ) 
+} 
 
-app.listen(3000);
-console.log('you are listening to port 3000');
+// save code as start.js 
